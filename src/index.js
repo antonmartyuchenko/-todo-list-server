ba3246e89b83dd2e045fb86216c32e25261e3a78
Мартюченко Антон Владимирович <mav@beltransways.local>
2020-04-07 13:29:05 +0300

    v6

--- src/index.js
+++ src/index.js
@@ -1,120 +1,98 @@
 const fs = require('fs');
 const args = require('yargs').argv;
-const method = args.method;
+
+const { method } = args;
 
 const addMessage = (message) => {
+  if (typeof message !== 'string' || message === '') { throw new Error('Enter your message'); }
 
-    if (typeof message !== 'string' || message === '') { throw new Error('Enter your message') };
+  let lineBreak = '';
 
-    let lineBreak = '';
+  if (fs.existsSync('log.txt') && fs.readFileSync('log.txt', 'utf8') !== '') {
+    lineBreak = '\n';
+  }
 
-    if (fs.existsSync('log.txt') && fs.readFileSync('log.txt', 'utf8') !== '') {
-        lineBreak = '\n';
-    }
+  fs.appendFileSync('log.txt', `${lineBreak}${message}`);
 
-    fs.appendFileSync('log.txt', `${lineBreak}${message}`);
-
-    return message;
-
-}
+  return message;
+};
 
 const findAll = () => {
+  if (fs.existsSync('log.txt')) {
+    const fileData = fs.readFileSync('log.txt', 'utf8');
 
-    if (fs.existsSync('log.txt')) {
-
-        const fileData = fs.readFileSync('log.txt', 'utf8');
-
-        if (fileData) {
-            return fileData.split('\n');
-        }
+    if (fileData) {
+      return fileData.split('\n');
     }
+  }
 
-    return [];
-
-}
+  return [];
+};
 
 const findOne = id => {
+  if (fs.existsSync('log.txt')) {
+    const fileData = fs.readFileSync('log.txt', 'utf8');
 
-    if (fs.existsSync('log.txt')) {
+    if (fileData) {
+      const message = fileData.split('\n')[id];
 
-        const fileData = fs.readFileSync('log.txt', 'utf8');
+      if (!message) {
+        throw new Error('Message does not exist');
+      }
 
-        if (fileData) {
-
-            let message = fileData.split('\n')[id];
-
-            if (!message) {
-                throw new Error('Message does not exist') 
-            }
-               
-            return message
-            
-        }
+      return message;
     }
+  }
 
-    return '';
-
-}
+  return '';
+};
 
 const deleteMessage = id => {
+  if (fs.existsSync('log.txt')) {
+    const fileData = fs.readFileSync('log.txt', 'utf8');
 
-    if (fs.existsSync('log.txt')) {
+    if (fileData) {
+      const messages = fileData.split('\n');
 
-        const fileData = fs.readFileSync('log.txt', 'utf8');
+      if (id >= messages.length) {
+        throw new Error('Message does not exist');
+      }
 
-        if (fileData) {
+      messages.splice(id, 1);
 
-            let messages = fileData.split('\n');
+      fs.writeFileSync('log.txt', messages.join('\n'));
+    }
+  }
+};
 
-            if (id >= messages.length) {
-                throw new Error('Message does not exist') 
-            }
-             
-            messages.splice(id, 1);
+if (typeof method === 'string') {
+  if (method.toLowerCase() === 'post') {
+    addMessage(args.message);
+  } else if (method.toLowerCase() === 'get') {
+    const { id } = args;
 
-            fs.writeFileSync('log.txt', messages.join('\n'));
-   
-        }
+    if (id === true || id < 0) {
+      throw new Error('Enter id message');
     }
 
-}
-
-if (typeof method == 'string') {
-
-    if (method.toLowerCase() === 'post') {
-
-        addMessage(args.message);
-
-    } else if (method.toLowerCase() === 'get') {
-
-        let id = args.id;
-
-        if (id === true || id < 0) {
-            throw new Error('Enter id message') 
-        }
-
-        if (id || id === 0) {   
-            console.log(findOne(id));
-        } else {
-            console.log(findAll());
-        }
-
-    } else if (method.toLowerCase() === 'delete') { 
-    
-        let id = args.id;
-
-        if (id === true || id < 0) {
-            throw new Error('Enter id message') 
-        }
-
-        if (id || id === 0) {   
-            deleteMessage(id);
-        }
-
+    if (id || id === 0) {
+      console.log(findOne(id));
     } else {
-        throw new Error('To send messages use the method "POST" or method "GET" to get messages or method "DELETE" to delete messages');
+      console.log(findAll());
+    }
+  } else if (method.toLowerCase() === 'delete') {
+    const { id } = args;
+
+    if (id === true || id < 0) {
+      throw new Error('Enter id message');
     }
 
+    if (id || id === 0) {
+      deleteMessage(id);
+    }
+  } else {
+    throw new Error('To send messages use the method "POST" or method "GET" to get messages or method "DELETE" to delete messages');
+  }
 } else {
-    throw new Error('Enter method: POST or GET');
+  throw new Error('Enter method: POST or GET');
 }
